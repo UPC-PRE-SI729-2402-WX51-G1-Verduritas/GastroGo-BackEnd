39f7e0559d2d55b72f1cc9695a15b6b74c552328
Ricardo <richiabv@gmail.com>
2024-10-19 14:31:36 -0500

    chore: initial commit

---
+++ src/main/java/pe/upc/gastrogobackend/GastrogoBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.gastrogobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrogoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrogoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/upc/gastrogobackend/GastrogoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.gastrogobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrogoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
