8819d56321f9b17df5c036fc2c811be1d0a3c8db
Rendery77 <rendery77@gmail.com>
2024-10-19 22:14:10 -0500

    feat(shared): create entity PaymentInfo

---
+++ src/main/java/pe/upc/gastrogobackend/shared/domain/model/entities/PaymentInfo.java
@@ -0,0 +1,23 @@
+package pe.upc.gastrogobackend.shared.domain.model.entities;
+
+/**
+ * @summary PaymentInfo stores details about payment methods.
+ */
+public class PaymentInfo {
+    private String bankId;
+    private String bankEntityId;
+    private String bankCard;
+
+    /**
+     * @summary Processes payment.
+     */
+    public void makePayment() {
+    }
+
+    /**
+     * @summary Generates information about the payment method.
+     */
+    public void generateInfo() {
+    }
+
+}
