cb20fe7ec5e8de461e4e8cb53ed8ba48c90cc1d8
Rendery77 <rendery77@gmail.com>
2024-10-19 22:14:40 -0500

    feat(shared): create entity Restaurant

---
+++ src/main/java/pe/upc/gastrogobackend/shared/domain/model/entities/Restaurant.java
@@ -0,0 +1,19 @@
+package pe.upc.gastrogobackend.shared.domain.model.entities;
+
+/**
+ * @summary Restaurant represents a restaurant entity in the system.
+ */
+public class Restaurant {
+    private String restaurantId;
+    private String restaurantName;
+    private String phone;
+    private String email;
+    private String password;
+
+    /**
+     * @summary Modifies restaurant data.
+     */
+    public void modifyData() {
+    }
+
+}
