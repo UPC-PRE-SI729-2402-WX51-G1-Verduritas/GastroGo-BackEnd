687cb27b7d4913a77d1d9fc3e863cc546829aa9b
Rendery77 <rendery77@gmail.com>
2024-10-19 22:15:20 -0500

    feat(shared): create entity User

---
+++ src/main/java/pe/upc/gastrogobackend/shared/domain/model/entities/User.java
@@ -0,0 +1,28 @@
+package pe.upc.gastrogobackend.shared.domain.model.entities;
+
+/**
+ * @summary User represents an individual who uses the system.
+ */
+public class User {
+    private String userId;
+    private String dniUser;
+    private String name;
+    private String lastName;
+    private String address;
+    private String userType;
+    private String email;
+    private String password;
+
+    /**
+     * @summary Registers a new user.
+     */
+    public void register() {
+    }
+
+    /**
+     * @summary Logs in a user.
+     */
+    public void logIn() {
+    }
+
+}
