ee028683ee4808a806f9c57a67eb6e5db3f01c4d
FABIOLA <ximemegan1302@gmail.com>
2024-10-19 23:55:29 -0500

    feat(shared): added snake class with pluralized table

---
+++ src/main/java/pe/upc/gastrogobackend/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeClassWithPluralizedTablePhysicalNamingStrategy.java
@@ -0,0 +1,34 @@
+package pe.upc.gastrogobackend.shared.infrastructure.persistence.jpa.configuration.strategy;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class SnakeClassWithPluralizedTablePhysicalNamingStrategy implements PhysicalNamingStrategy {
+    @Override
+    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+    @Override
+    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+        return null;
+    }
+
+
+}
