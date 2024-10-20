bbb428196a1f785949944c4e1b647db72f70d2ee
Rendery77 <rendery77@gmail.com>
2024-10-19 22:13:12 -0500

    feat(shared): create entity Bill

---
+++ src/main/java/pe/upc/gastrogobackend/shared/domain/model/entities/Client.java
@@ -0,0 +1,26 @@
+package pe.upc.gastrogobackend.shared.domain.model.entities;
+
+/**
+ * @summary Client represents a user who can place orders.
+ */
+public class Client extends User {
+
+    /**
+     * @summary Modifies user preferences.
+     */
+    public void modifyPreferences() {
+    }
+
+    /**
+     * @summary Creates a booking for the client.
+     */
+    public void createBooking() {
+    }
+
+    /**
+     * @summary Registers payment details for the client.
+     */
+    public void registerPayment() {
+    }
+
+}
